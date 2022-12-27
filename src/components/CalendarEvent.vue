92fa64b6915a81c177c4f284ea1181789ef8011f
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-27 16:00:20 +0300

    Cleanup look of events

--- src/components/CalendarEvent.vue
+++ src/components/CalendarEvent.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="card">
-    <div class="card-content">{{ event.details }}</div>
-    <footer class="card-footer">
-      <button class="button is-primary card-footer-item is-radiusless">
+  <div class="card is-radiusless">
+    <div class="card-content is-radiusless">{{ event.details }}</div>
+    <footer class="card-footer is-borderless">
+      <button class="button is-success card-footer-item is-radiusless is-small">
         <font-awesome-icon icon="floppy-disk" />
       </button>
-      <button class="button is-warning card-footer-item is-radiusless">
+      <button class="button is-info card-footer-item is-radiusless is-small">
         <font-awesome-icon icon="fa-pen-to-square" />
       </button>
-      <button class="button is-danger card-footer-item is-radiusless">
+      <button class="button is-danger card-footer-item is-radiusless is-small">
         <font-awesome-icon icon="fa-trash" />
       </button>
     </footer>
   </div>
+  <div></div>
 </template>
 
 <script>
@@ -22,4 +23,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+button {
+  border: none;
+}
 </style>
