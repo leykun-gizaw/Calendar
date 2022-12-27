8d8305f5b257b1e54f8c4771654f58814073f1a3
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-27 16:32:25 +0300

    Update default card values to boilerplate entry content

--- src/components/CalendarEntry.vue
+++ src/components/CalendarEntry.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="card">
     <div class="card-content">
-      <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-        iaculis mauris.
+      <input
+        class="is-radiusless is-borderless"
+        type="text"
+        name=""
+        id=""
+        placeholder="New Event"
+      />
+      <div class="content has-text-primary">
+        Day of event: <strong class="has-text-primary"><em>Monday</em></strong>
       </div>
+      <footer>
+        <button class="button is-primary is-outlined">Submit</button>
+      </footer>
     </div>
   </div>
 </template>
@@ -15,7 +24,24 @@ export default {};
 
 <style lang="scss" scoped>
 .card {
-  width: 30%;
+  width: 25%;
   margin: 50px auto;
+}
+.card input {
+  border: none;
+  border-bottom: 1px solid #ccc;
+  padding: 15px 0;
+  width: 100%;
+}
+.card input::placeholder {
+  font-size: 1.75rem;
+  color: #ccc;
+}
+.card .content {
+  text-align: left;
+  margin: 20px 0;
+}
+footer {
+  text-align: left;
 }
 </style>
