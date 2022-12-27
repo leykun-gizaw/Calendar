7af9794af7e33919eb9ae09fa6d95c518be6a43b
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-27 12:05:46 +0300

    Import bulma CSS npm module

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
+import '../node_modules/bulma/css/bulma.css';
 import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
