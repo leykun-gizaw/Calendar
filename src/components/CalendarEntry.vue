d3e490006a311e7522264ba48bb8c54e92c22aee
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-27 16:11:52 +0300

    Make event entry card in CalendaryEntry component

--- src/components/CalendarEntry.vue
+++ src/components/CalendarEntry.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="calendar-entry"></div>
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+        iaculis mauris.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.card {
+  width: 30%;
+  margin: 50px auto;
+}
 </style>
