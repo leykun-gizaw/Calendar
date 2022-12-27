a5e9de40af86b09730ec37137098d31713f7bd5b
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-27 13:05:50 +0300

    Darken background and center text in card header

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="column">
-    <div class="card is-radiusless">
-      <header class="card-header">
+    <div class="card is-radiusless is-shadowless">
+      <header class="card-header is-shadowless">
         <p class="card-header-title">Component</p>
       </header>
       <div class="card-content"></div>
@@ -23,5 +23,11 @@ export default {
 }
 .card {
   min-width: 200px;
+  border-left: 1px solid #ddd;
+}
+.card-header-title {
+  background-color: #555;
+  color: white;
+  justify-content: center;
 }
 </style>
