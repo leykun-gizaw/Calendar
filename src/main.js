c7f98838ab3fd73788f2a9c315cbb073cbbb73b2
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-27 10:50:23 +0300

    Import library and renderer for fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).mount('#app')
+library.add();
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
