9433a82cbaff1ab6bf273c0e4ae17f3d90eefd84
Leykun Gizaw <leykungizaw@gmail.com>
2022-12-29 11:11:41 +0300

    Change main css file to sass

--- src/components/CalendarEvent.vue
+++ src/components/CalendarEvent.vue
@@ -31,7 +31,6 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  width: 9rem;
   text-align: center;
 }
 .card .card-content {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import './style.css';
+import './style.scss';
 import '../node_modules/bulma/css/bulma.css';
 import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
